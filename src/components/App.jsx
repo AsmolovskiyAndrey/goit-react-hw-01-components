e9e40ad12d09a582d5c4f41aa191da1b036b82f1
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-12-13 01:16:19 +0100

    make2block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import { Profile } from "./Profile/Profile";
-import user from 'data/user'
+import { Statistics } from "./Statistics/Statistics";
+import user from 'data/user';
+import data from 'data/data';
 
 export const App = () => {
   return (
     <div>
     <Profile username={user.username}
-              tag={user.tag}
-              location={user.location}
-              avatar={user.avatar}
-              stats={user.stats} />
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} />
+    <Statistics title="Upload stats" stats={data} />
+    <Statistics stats={data} />
     </div>
   );
 };
