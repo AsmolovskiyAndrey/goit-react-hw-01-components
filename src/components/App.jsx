78746851146ea5c82493b530c40f05cebd374c3d
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-12-13 22:25:43 +0100

    all_block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
-import { Profile } from "./Profile/Profile";
-import { Statistics } from "./Statistics/Statistics";
-import { FriendList } from "./FriendList/FriendList";
+import { Profile } from "components/Profile/Profile";
+import { Statistics } from "components//Statistics/Statistics";
+import { FriendList } from "components//FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 import user from 'data/user';
 import data from 'data/data';
 import friends from 'data/friends';
+import transactions from 'data/transactions';
 
 export const App = () => {
   return (
@@ -15,7 +17,8 @@ export const App = () => {
       stats={user.stats} />
     <Statistics title="Upload stats" stats={data} />
     <Statistics stats={data} />
-    <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
