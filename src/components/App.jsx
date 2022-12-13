0cb5cce0ab57fdfdb7deff4507212bc567b71415
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-12-13 20:47:20 +0100

    third_block_ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
 import user from 'data/user';
 import data from 'data/data';
+import friends from 'data/friends';
 
 export const App = () => {
   return (
-    <div>
+    <>
     <Profile username={user.username}
       tag={user.tag}
       location={user.location}
@@ -13,6 +15,7 @@ export const App = () => {
       stats={user.stats} />
     <Statistics title="Upload stats" stats={data} />
     <Statistics stats={data} />
-    </div>
+    <FriendList friends={friends}/>
+    </>
   );
 };
